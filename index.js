51f65f536162789e595573a20d9a8fa1a01aaba7
Romain Lenzotti <rom.lenzotti@gmail.com>
2015-05-14 10:12:57 +0200

    Initiale commit

---
+++ index.js
@@ -0,0 +1,62 @@
+var log = true;
+var colors = require('colors/safe');
+
+GLOBAL.$log = module.exports = {
+    /**
+     * Start logguer
+     */
+    start:function(){
+        log = true;
+    },
+    /**
+     * Stop logguer
+     */
+    stop:function(){
+        log = false;
+    },
+    /**
+     * Print a trace in the console.
+     * @returns {*}
+     */
+    debug:function(){
+        if(log){
+            var o = [colors.blue('[INFO]')];
+
+            for(var key in arguments){
+                o.push(arguments[key]);
+            }
+
+            return console.log.apply(console, o);
+        }
+    },
+    /**
+     *
+     * @returns {*}
+     */
+    warn:function(){
+        if(log){
+            var o = [colors.yellow('[WARN]')];
+
+            for(var key in arguments){
+                o.push(arguments[key]);
+            }
+
+            return console.warn.apply(console, o);
+        }
+    },
+    /**
+     *
+     * @returns {*}
+     */
+    error:function(){
+        if(log){
+            var o = [colors.red('[ERROR]')];
+
+            for(var key in arguments){
+                o.push(arguments[key]);
+            }
+
+            return console.warn.apply(console, o);
+        }
+    }
+};
